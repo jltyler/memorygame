a50506a1a645d3a1bc2b4bdcb69193d6e88ce710
Sumry <john.lucas.tyler@gmail.com>
2017-06-03 14:47:39 -0600

    Added some test game logic.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 console.log("Running...");
 
-var cardOne = "king"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "queen"
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+
+flipA = cards[0];
+cardsInPlay.push(flipA);
+console.log("User flipped " + flipA);
+
+flipB = cards[1];
+cardsInPlay.push(flipB);
+console.log("User flipped " + flipB);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!")
+  } else {
+    alert("Sorry, try again.")
+  }
+}
