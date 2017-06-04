cd7b1a8b259210394210d5c4c41b4347c98d44d0
Sumry <john.lucas.tyler@gmail.com>
2017-06-03 21:23:20 -0600

    Added functions cardsInPlay is cleared after two cards are flipped

--- js/main.js
+++ js/main.js
@@ -3,18 +3,19 @@ console.log("Running...");
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
 
-flipA = cards[0];
-cardsInPlay.push(flipA);
-console.log("User flipped " + flipA);
-
-flipB = cards[1];
-cardsInPlay.push(flipB);
-console.log("User flipped " + flipB);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!")
-  } else {
-    alert("Sorry, try again.")
+function checkMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+    cardsInPlay = [];
   }
+}
+
+function flipCard(ind) {
+  cardsInPlay.push(cards[ind]);
+  console.log("User flipped " + cards[ind] + " (cards[" + ind + "])");
+  checkMatch();
 }
