c1c28985ff9a0d0df0b6998d3159d06264f35b95
Sumry <john.lucas.tyler@gmail.com>
2017-06-05 11:01:26 -0600

    Added basic logic and events

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 console.log("Running...");
 
+// Card definitions
 cards = [
   {
     suit: 'hearts',
@@ -20,25 +21,42 @@ cards = [
     suit: 'diamonds',
     rank: 'king',
     cardImage: 'images/king-of-diamonds.png'
-  },
-
+  }
 ];
 cardsInPlay = [];
 
+// Check if cardsInPlay contains a match
 function checkMatch() {
-  if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
+  if (cardsInPlay.length >= 2) {
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
       alert('You found a match!');
     } else {
       alert("Sorry, try again.");
     }
-    cardsInPlay = [];
+    cardsInPlay = []; // Clear the cardsInPlay array
   }
 }
 
-function flipCard(ind) {
-  var c = cards[ind]
+// Flip card to hook into click event
+function flipCard() {
+  var i = this.getAttribute('data-id');
+  var c = cards[i];
   cardsInPlay.push(c);
+  this.setAttribute('src', c.cardImage)
   console.log('User flipped ' + c.rank + ' of ' + c.suit);
   checkMatch();
 }
+
+function createBoard() {
+  var game = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++) {
+    var new_card = document.createElement('img');
+    new_card.setAttribute('src', 'images/back.png');
+    new_card.setAttribute('data-id', i);
+    new_card.addEventListener('click', flipCard);
+    game.appendChild(new_card);
+  }
+}
+
+// Time to run the actual code
+createBoard()
